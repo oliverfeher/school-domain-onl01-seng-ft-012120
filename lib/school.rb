ac2ebdd985f26c13b7be14833e4db4ec57711650
Oliver Feher <olcsee@gmail.com>
2020-01-28 16:21:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,17 @@
 class School
+
   attr_reader :roster, :name #have to create reader for name
   
-  def initialize #initialize with name, but must create reader
+  def initialize(name) #initialize with name, but must create reader
     @name = name 
     @roster = {}
   end
-  
+
   def add_student(student_name, grade)
    if @roster.has_key?(grade)
     @roster[grade] << student_name
     else
     @roster[grade] = [student_name]
+    end
   end
 end
