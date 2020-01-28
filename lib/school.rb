1b8e6201ef93ae8df37ee8eb4bc213539eaa6be2
Oliver Feher <olcsee@gmail.com>
2020-01-28 16:44:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_reader :roster, :name #have to create reader for name
+  
+  def initialize(name) #initialize with name, but must create reader
+    @name = name 
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+   if @roster.has_key?(grade)
+    @roster[grade] << student_name
+    else
+    @roster[grade] = [student_name]
+    end
+  end
+
+  def grade(grade)
+    if roster.include?(grade)
+      roster[grade].map { |student| student}
+    end
+  end
+
+  def sort
+    @roster.map { |key, value| [key, value.sort]}.to_h
+  end
+
+end
